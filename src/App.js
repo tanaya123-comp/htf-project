ce2d0e3db34dcf2e7cee297eec46f61b8ddc472a
tanaya123 <tanayawankar58@gmail.com>
2021-10-23 14:59:48 +0530

    Some more changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Events from "./components/Events";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
@@ -11,9 +13,13 @@ function App() {
       <Navbar />
      
       <Router>
-        <Switch>
-          <Route exact path="/" component={Dashboard}></Route>
+
+      <Switch>
+        <Route exact path='/' component={Dashboard}></Route>
+        <Route exact path='/Leaderboard' component={Leaderboard}></Route>
+          <Route exact path='/events' component={Events}></Route>
         </Switch>
+
       </Router>
       </header>
     </div>
